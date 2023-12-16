3f0a6d9935c1902f293ccf020f3c957e34ee7979
DMNihal65 <Nihaldm65@gmail.com>
2023-12-16 19:19:38 +0530

    First assignment anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,84 @@
 */
 
 function isAnagram(str1, str2) {
+  let flag = false;
+  console.log(str1.length, str2.length)
+  if(str1.length != str2.length){
+    return false;
+  }
+  let str1array = [...str1.toLowerCase()]
+  let str2array = [...str2.toLowerCase()]
+  str1array.sort()
+  str2array.sort() 
+  console.log(str1array) 
+  console.log(str2array) 
+
+  for(let i=0;i<=str1array.length;i++){
+    if(str1array[i] == str2array[i]){
+      // console.log("Anagram")
+
+      flag = true
+    }
+    else{
+      // console.log("Not an Anagram")
+      flag = false
+      break;
+    }
+  }
+
+  if(flag){
+    // console.log("Anagram")
+    return true
+  }
+  else{
+    // console.log("Not and Anagram")
+    return false
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // for(let i=0;i<str1array.length;i++){
+  //   for(let j=0;j<str2array.length;j++){
+  //     if(str1array[i] == str2array[j]){
+  //        flag = true;
+  //        break;
+  //     }
+  //     else{
+  //       flag = false;
+  //       break;
+  //     }
+  //   }
+  // }
+
+  // if(flag){
+  //   console.log("Anagram")
+  //   return true;
+  // }
+  // else{
+  //   console.log("Not an anagram")
+  //   return false;
+  // }
 
 }
+
+
+
+// isAnagram("", "")
 
 module.exports = isAnagram;
